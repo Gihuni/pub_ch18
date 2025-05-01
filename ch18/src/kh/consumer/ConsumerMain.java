1ba8734b6f0b12cf5e640e20e3fcfdb0cf367696
Huni <huni971209@naver.com>
2025-05-01 10:27:28 +0900

    람다식 consumer 처리 테스팅 완료

--- ch18/src/kh/consumer/ConsumerMain.java
+++ ch18/src/kh/consumer/ConsumerMain.java
@@ -1,6 +1,8 @@
 package kh.consumer;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
+import java.util.function.IntConsumer;
 
 public class ConsumerMain {
 
@@ -12,18 +14,28 @@ public class ConsumerMain {
 		con.accept("홍길동");
 		con.accept("구길동");
 		con.accept("저길동");
-
-		Consumer<Integer> conInt = t -> {
-			for (int i = 1; i <= 9; i++) {
-				System.out.printf("%d * %d = %d\n", t, i, t * i);
+		// =================================================================
+		Consumer<Integer> con2 = t -> {
+			System.out.printf("%d단 출력\n", t);
+			for (int i = 0; i < 9; i++) {
+				System.out.printf("%d * %d = %d\n", t, i + 1, t * (i + 1));
 			}
-
 		};
-
-		conInt.accept(6);
-
+		con2.accept(5);
+		// =================================================================
+		IntConsumer icon = t -> {
+			System.out.printf("%d단 출력\n", t);
+			for (int i = 0; i < 9; i++) {
+				System.out.printf("%d * %d = %d\n", t, i + 1, t * (i + 1));
+			}
+		};
+		icon.accept(7);
+		// =================================================================
+		BiConsumer<String, Integer> bicon = (name, age) -> 
+		System.out.printf("%s님 나이는 %d 입니다.\n", name, age);
+		bicon.accept("김동진", 60);
+		bicon.accept("이해연", 23);
+		
 		System.out.println("The end");
-
 	}
-
 }
